47061b4d8ada4ea3904f52c3fd242c1c86272616
Masanori Tani <uta8a@uta8a.org>
2023-08-23 12:24:53 +0000

    wip

--- server/main.go
+++ server/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 
@@ -11,30 +12,34 @@ import (
 )
 
 func main() {
+	baseUrl := os.Getenv("BASE_URL")
 	conf := &oauth2.Config{
 		ClientID:     os.Getenv("GITHUB_CLIENT_ID"),
 		ClientSecret: os.Getenv("GITHUB_CLIENT_SECRET"),
-		RedirectURL:  "http://127.0.0.1:8080/api/auth/callback",
+		RedirectURL:  fmt.Sprintf("%s/api/auth/callback", baseUrl),
 		Scopes:       []string{}, // 最小限。public dataのみを取得
 		Endpoint:     github.Endpoint,
 	}
-	url := conf.AuthCodeURL("state")
+	url := conf.AuthCodeURL("github_auth")
 	fmt.Printf("Visit the URL for the auth dialog: %v\n", url)
 
 	var authCode string
 	fmt.Scan(&authCode)
 	fmt.Printf("%v\n", authCode)
 	// Handle the exchange code to initiate a transport.
-	tok, err := conf.Exchange(oauth2.NoContext, authCode)
+	tok, err := conf.Exchange(context.TODO(), authCode)
 	if err != nil {
 		log.Fatal(err)
 	}
-	client := conf.Client(oauth2.NoContext, tok)
+	client := conf.Client(context.TODO(), tok)
 	res, err := client.Get("https://api.github.com/user")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("%s\n", body)
 }
